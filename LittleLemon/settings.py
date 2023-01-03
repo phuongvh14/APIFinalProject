27a8298ee71450962bf6dd753aab62e33c2523cb
Phuong Vu <hhanhphuongg14@gmail.com>
2023-01-03 17:28:55 +0700

    Finalize user generation and token generation endpoints

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Category, MenuItem, Cart, Order, OrderItem
 
 # Register your models here.
-admin.site.register(Category, MenuItem, Cart, Order, OrderItem)
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(Cart)
+admin.site.register(Order) 
+admin.site.register(OrderItem)
